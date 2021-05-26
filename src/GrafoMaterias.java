bea383e58eb8e9bedafeb7288f3363c7c76d1f45
IsaacIpenza <benjamin.ipre@gmail.com>
2021-05-26 01:49:31 -0500

    Implementacion de metodos de Orden Topologico
    Se realizaron dos posibles metodos de orden topologico de los cuales
    solo uno es factible para el problema de las materias a cursar.
    	- ordenTopologico()
    	- ordenTopologicoDirecto()
    
    Se implemento una prueba del programa a nivel de Consola.

--- src/GrafoMaterias.java
+++ src/GrafoMaterias.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class GrafoMaterias {
 	
@@ -7,8 +9,7 @@ public class GrafoMaterias {
 	private int size;
 	private String[] nombreVertice;
 	private Hashtable<String, Integer> posVertice;
-	
-	private Hashtable<String, String> clave_materia;
+	private Hashtable<String, String> clave_materia; // <Clave, Materia>
 
 	public GrafoMaterias(int numeroDeMaterias) {
 		this.size = 0;
@@ -16,17 +17,6 @@ public class GrafoMaterias {
 		this.nombreVertice = new String[numeroDeMaterias];
 		this.posVertice = new Hashtable<>();
 		this.clave_materia = new Hashtable<>();
-	}
-	
-	public void agregarDatos(ArrayList<Materia> materiasList) {
-		
-		for (Materia materia : materiasList) {
-			agregarVertice(materia);
-		}
-	
-		for (Materia materia : materiasList) {
-			agregarAristas(materia);
-		}
 	}
 	
 	public void agregarVertice(Materia materia) {
@@ -51,8 +41,100 @@ public class GrafoMaterias {
 		}
 	}
 	
-	public void ordenTopologico() {
+	public void agregarDatos(ArrayList<Materia> materiasList) {
 		
+		for (Materia materia : materiasList) {
+			agregarVertice(materia);
+		}
+	
+		for (Materia materia : materiasList) {
+			agregarAristas(materia);
+		}
 	}
 	
+	/**
+	 * Metodo que genera el orden topologico de manera directa tomando primero todas aquellas materias
+	 * que no tienen requisitos y luego las que ya se cumplio el requisitos y asi sucesivamente.
+	 * @return Un queue con el orden en el que las materias se deben de cursar.
+	 */
+	public Queue<String> ordenTopologico() {
+
+		Queue<String> ordenMaterias =  new LinkedList<String>();
+		boolean[] agregado = new boolean[this.size];
+		
+		boolean agregar;
+		int materiasAgregadas = 0;
+
+		while(materiasAgregadas < this.size) {
+			for (int i = 0; i < this.size; i++) {
+				agregar = true;
+				
+				if(!agregado[i]) {
+					for (int j = 0; j < this.size; j++) {
+						if(matriz[j][i] == 1) {  //iteramos los valores de las filas sobre una columna
+							agregar = false;
+							break;
+						}
+					}
+					
+					if(agregar) {
+						agregado[i] = true;
+						ordenMaterias.add(nombreVertice[i]);
+						materiasAgregadas++;
+					}
+				}
+			}
+			
+			for (int i = 0; i < this.size; i++) {
+				if(agregado[i]) {
+					for (int j = 0; j < this.size; j++) {
+						if(matriz[i][j] == 1) { //iteramos los valores de las columnas sobre una fila
+							matriz[i][j] = 0;;
+						}
+					}
+				}
+			}
+		}
+
+		return ordenMaterias;
+	}
+	
+	/**
+	 * Metodo que encuentra el orden topologico apartir de cursar las materias segun el orden del csv, 
+	 * peso si esta tiene algun requisito se prioriza el completar este requisitos para asi lograr
+	 * cursar la materia en la que se estaba.
+	 * No considera el no poder cursar materia y sus requisitos al mismo tiempo.
+	 * @return Un queue con el orden en el que las materias se deben de cursar.
+	 */
+	public Queue<String> ordenTopologicoDirecto() {
+		
+		Queue<String> ordenMaterias =  new LinkedList<String>();
+		boolean[] agregado = new boolean[this.size];
+		
+		for (int x = 0; x < agregado.length; x++) {
+			for (int i = 0; i < this.size; i++) {
+				ordenTopologicoRec(ordenMaterias, i, agregado);
+			}
+		}
+		
+		return ordenMaterias;
+	}
+	
+	public void ordenTopologicoRec(Queue<String> ordenMaterias, int i, boolean[] agregado) {
+		
+		if(!agregado[i]) {
+			
+			for (int j = 0; j < this.size; j++) {
+				//Iteramos las filas de una misma columan
+				if(matriz[j][i] == 1) {
+					ordenTopologicoRec(ordenMaterias, j, agregado);
+					matriz[j][i] = 0;
+					return;
+				}
+			}
+			
+			agregado[i] = true;
+			ordenMaterias.add(nombreVertice[i]);
+		}
+	}
 }
--- src/ProyectoMain.java
+++ src/ProyectoMain.java
@@ -1,23 +1,46 @@
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Queue;
+
+import javax.swing.JOptionPane;
 
 public class ProyectoMain {
 
 	public static void main(String[] args) throws FileNotFoundException, Exception {
 		
-		String fileName = "IMT.csv";
+		String fileName = "IMT_invertido.csv";
 		String[] headers = {"Clave","Materia","Requisitos"};
 		
 		CsvMaterias csvMaterias = new  CsvMaterias(fileName, headers);
 		ArrayList<Materia> materiasList = csvMaterias.extraerInformacion();
 		
-		GrafoMaterias grafo = new GrafoMaterias(materiasList.size());
-		grafo.agregarDatos(materiasList);
+		GrafoMaterias grafoMaterias = new GrafoMaterias(materiasList.size());
+		grafoMaterias.agregarDatos(materiasList);
 		
 		System.out.println("Materias cargadas exitosamente!!!");
+		System.out.println();
+
+		Queue<String> ordenMaterias = grafoMaterias.ordenTopologicoDirecto();
 		
+		System.out.println();
+		System.out.println("Orden de las Materias");
+		System.out.println();
 		
+		String materiasPorSemestre = JOptionPane.showInputDialog("¿Cuantas materias se cursaran por semestre?", 7);
+		int MPS = Integer.parseInt(materiasPorSemestre);
 		
+		int semestre = 1;
+		while(!ordenMaterias.isEmpty()) {
+			
+			System.out.println();
+			System.out.println(" \t" + semestre + "° Semestre");
+			
+			for (int i = 0; i < MPS && !ordenMaterias.isEmpty(); i++) {
+				System.out.println("--> " + ordenMaterias.poll());
+			}
+			
+			semestre++;
+		}
 	}
 
 	
